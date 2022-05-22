f5cf22c12d7c48a5d00ecd51522e84a49293a396
smabuk <2011834+smabuk@users.noreply.github.com>
2022-05-22 13:36:42 +0100

    v1.0.1 DateTimeExtensions expanded

--- src/Smab.Helpers/DateTimeExtensions.cs
+++ src/Smab.Helpers/DateTimeExtensions.cs
@@ -2,7 +2,11 @@
 
 public static class DateTimeExtensions
 {
+    internal static readonly string DD_MMM_YYYY = "dd MMM, yyyy";
     internal static readonly string DD_MMMM_YYYY = "dd MMMM, yyyy";
 
 	public static string ToDateLongMonthYearString(this DateTime dateTime) => dateTime.ToString(DD_MMMM_YYYY);
+	public static string ToDateLongMonthYearString(this DateOnly dateOnly) => dateOnly.ToString(DD_MMMM_YYYY);
+	public static string ToDateShortMonthYearString(this DateTime dateTime) => dateTime.ToString(DD_MMM_YYYY);
+	public static string ToDateShortMonthYearString(this DateOnly dateOnly) => dateOnly.ToString(DD_MMM_YYYY);
 }
