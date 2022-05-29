a449e0e7b2933b07d3ee22adb92f370508b78bf6
smabuk <2011834+smabuk@users.noreply.github.com>
2022-05-29 17:48:12 +0100

    Add tests

---
+++ test/Smab.Helpers.Test/HelperMethodTests/DataInputCleanupTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Smab.Helpers.Test.HelperMethodTests;
+
+public class DataInputCleanupTest {
+	[Theory]
+	[InlineData(new string[] { "Line 1", "" }, new string[] { "Line 1" })]
+	[InlineData(new string[] { "Line 1", "Line 2" }, new string[] { "Line 1", "Line 2" })]
+	[InlineData(null, new string[0])]
+	public void Should_RemoveBlankLineFromEnd_IfLineIsEmpty(string[] input, string[] expected) {
+		string[] actual = DataInputCleanup.StripTrailingBlankLineOrDefault(input);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData(new string[] { "Line 1", "" }, new string[] { "Line 1" })]
+	[InlineData(new string[] { "Line 1", "Line 2" }, new string[] { "Line 1", "Line 2" })]
+	[InlineData(null, new string[0])]
+	public void Should_StripTrailingBlankLineOrDefault_DotSyntax(string[] input, string[] expected) {
+		string[] actual = input.StripTrailingBlankLineOrDefault();
+		Assert.Equal(expected, actual);
+	}
+}
