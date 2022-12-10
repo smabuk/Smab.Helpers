992b619705b84353887032df318835116a6ce193
smabuk <2011834+smabuk@users.noreply.github.com>
2022-12-10 17:17:55 +0000

    Ocr WIP

---
+++ test/Smab.Helpers.Test/HelperMethodTests/OcrHelperTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Smab.Helpers.Test.HelperMethodTests;
+public class OcrHelperTests
+{
+	[Theory]
+	[InlineData("""
+		.##..###...##..###..####.####..##..#..#.###...##.#..#.#.....##..###..###...###.#..#.#...#.####.
+		#..#.#..#.#..#.#..#.#....#....#..#.#..#..#.....#.#.#..#....#..#.#..#.#..#.#....#..#.#...#....#.
+		#..#.###..#....#..#.###..###..#....####..#.....#.##...#....#..#.#..#.#..#.#....#..#..#.#....#..
+		####.#..#.#....#..#.#....#....#.##.#..#..#.....#.#.#..#....#..#.###..###...##..#..#...#....#...
+		#..#.#..#.#..#.#..#.#....#....#..#.#..#..#..#..#.#.#..#....#..#.#....#.#.....#.#..#...#...#....
+		#..#.###...##..###..####.#.....###.#..#.###..##..#..#.####..##..#....#..#.###...##....#...####.
+		""", OcrHelpers.OcrLetterSize.Normal, "ABCDEFGHIJKLOPRSUYZ")]
+	public void OcrAlphabet(string ocrAlphabet, OcrHelpers.OcrLetterSize letterSize, string expected)
+	{
+		string actual = OcrHelpers.IdentifyMessage(ocrAlphabet, 1, letterSize);
+		Assert.Equal(expected, actual);
+	}
+
+}
