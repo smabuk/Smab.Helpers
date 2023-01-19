dc6ad66cc99a44d332d69e04be9485a3c55fba39
smabuk <2011834+smabuk@users.noreply.github.com>
2023-01-19 21:53:41 +0000

    ConsoleHelpers
    - KeyReader
    - WriteRow

---
+++ src/Smab.Helpers/ConsoleHelpers.cs
@@ -0,0 +1,59 @@
+﻿namespace Smab.Helpers;
+
+public class ConsoleHelpers {
+
+	public static class KeyReader {
+		private static readonly Thread inputThread;
+		private static readonly AutoResetEvent getKey;
+		private static readonly AutoResetEvent gotKey;
+		private static ConsoleKey key;
+
+		static KeyReader() {
+			getKey = new AutoResetEvent(false);
+			gotKey = new AutoResetEvent(false);
+			inputThread = new Thread(Reader) {
+				IsBackground = true
+			};
+			inputThread.Start();
+		}
+
+		private static void Reader() {
+			while (true) {
+				getKey.WaitOne();
+				key = Console.ReadKey(true).Key;
+				gotKey.Set();
+			}
+		}
+
+		public static ConsoleKey ReadKey(int timeOutMillisecs = Timeout.Infinite) {
+			getKey.Set();
+			bool success = gotKey.WaitOne(timeOutMillisecs);
+			return success ? key : ConsoleKey.Zoom;
+		}
+	}
+
+
+	public static void WriteRow(string message, int? col = null, int? row = null, bool clearToEndOfRow = true, ConsoleColor? colour = null) {
+		(int currentCol, int currentRow) = Console.GetCursorPosition();
+		currentCol = col ?? currentCol;
+		currentRow = row ?? currentRow;
+
+		if (clearToEndOfRow) {
+			Console.SetCursorPosition(currentCol, currentRow);
+			Console.Write(new string(' ', Console.WindowWidth - 1 - currentCol));
+		}
+
+		if (colour is not null) {
+			Console.ForegroundColor = (ConsoleColor)colour;
+		}
+
+		Console.SetCursorPosition(currentCol, currentRow);
+		Console.Write(message);
+
+		if (colour is not null) {
+			Console.ResetColor();
+		}
+	}
+
+
+}
