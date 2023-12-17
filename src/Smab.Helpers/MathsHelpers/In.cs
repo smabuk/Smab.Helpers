6f9f2f2a0406601f086ca4edd7bdaad1b48675e3
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-17 22:27:50 +0000

    v1.4.13 number.In()

---
+++ src/Smab.Helpers/MathsHelpers/In.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace Smab.Helpers;
+public static partial class MathsHelpers {
+	public static bool In<T>(this T value, IList<T> values) where T : INumber<T>
+		=> values.Contains(value);
+
+	public static bool In<T>(this T value, HashSet<T> values) where T : INumber<T>
+		=> values.Contains(value);
+
+	public static bool In<T>(this T value, IEnumerable<T> values) where T : INumber<T>
+		=> values.Contains(value);
+
+
+
+
+	public static bool InRange<T>(this T value, T minValue, T maxValueExclusive) where T : INumber<T>
+	=> minValue <= value && value < maxValueExclusive;
+
+	public static bool InRange<T>(this T value, (T MinValue, T MaxValueExclusive) range) where T : INumber<T>
+		=> range.MinValue <= value && value < range.MaxValueExclusive;
+
+}
