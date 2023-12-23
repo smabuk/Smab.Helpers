690928f9fb0a10416537bfaef23b994674a1acc5
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-23 15:15:27 +0000

    v1.5.4 new GetAdjacent cells overload

--- src/Smab.Helpers/GridHelpers/DoesNotContain.cs
+++ src/Smab.Helpers/GridHelpers/DoesNotContain.cs
@@ -3,10 +3,28 @@
 public static partial class ArrayHelpers {
 
 	public static bool DoesNotContain<TSource>(this IEnumerable<TSource> source, TSource value) =>
-		  !Enumerable.Contains(source, value, null);
+		source is ICollection<TSource> collection ? !collection.Contains(value) :
+		DoesNotContain(source, value, null);
 
-	public static bool DoesNotContain<TSource>(this IEnumerable<TSource> source, TSource value, IEqualityComparer<TSource>? comparer) =>
-		 !Enumerable.Contains(source, value, comparer);
+	public static bool DoesNotContain<TSource>(this IEnumerable<TSource> source, TSource value, IEqualityComparer<TSource>? comparer) {
 
+		ArgumentException.ThrowIfNullOrEmpty(nameof(source));
+
+		if (comparer is null) {
+			foreach (TSource element in source) {
+				if (EqualityComparer<TSource>.Default.Equals(element, value)) // benefits from devirtualization and likely inlining
+				{
+					return false;
+				}
+			}
+		} else {
+			foreach (TSource element in source) {
+				if (comparer.Equals(element, value)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
 }
-
