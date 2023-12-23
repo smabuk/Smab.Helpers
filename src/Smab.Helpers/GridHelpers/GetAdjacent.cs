b2adddc8ec801b98a1db0457b08b471b18004e65
smabuk <2011834+smabuk@users.noreply.github.com>
2023-12-23 15:31:09 +0000

    v1.5.5 revert GetAdjacentCells change

--- src/Smab.Helpers/GridHelpers/GetAdjacent.cs
+++ src/Smab.Helpers/GridHelpers/GetAdjacent.cs
@@ -19,23 +19,6 @@ public static partial class ArrayHelpers {
 		}
 	}
 
-	public static IEnumerable<Cell<T>> GetAdjacentCells<T>(this T[,] array, int x, int y, bool includeDiagonals = false, T[]? exclude = null) {
-		IEnumerable<(int dX, int dY)> DIRECTIONS = includeDiagonals switch {
-			true  => ALL_DIRECTIONS,
-			false => CARDINAL_DIRECTIONS,
-		};
-
-		foreach ((int dX, int dY) in DIRECTIONS) {
-			int newX = x + dX;
-			int newY = y + dY;
-			if (array.IsInBounds(newX, newY)) {
-				if (exclude is null || !exclude.Contains(array[newX, newY])) {
-					yield return new(newX, newY, array[newX, newY]);
-				}
-			}
-		}
-	}
-
 	public static IEnumerable<Cell<T>> GetAdjacentCells<T>(this T[,] array, (int x, int y) point, bool includeDiagonals = false, IEnumerable<(int dX, int dY)>? exclude = null)
 		=> GetAdjacentCells<T>(array, point.x, point.y, includeDiagonals, exclude);
 }
