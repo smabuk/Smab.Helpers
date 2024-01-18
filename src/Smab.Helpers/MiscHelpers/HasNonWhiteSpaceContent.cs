a5b9129eef70f211d11aa0b528154b32ab0c6954
smabuk <2011834+smabuk@users.noreply.github.com>
2024-01-18 12:05:15 +0000

    v1.5.9 HasNonWhiteSpaceContent

---
+++ src/Smab.Helpers/MiscHelpers/HasNonWhiteSpaceContent.cs
@@ -0,0 +1,4 @@
+﻿namespace Smab.Helpers;
+public static partial class StringHelpers {
+	public static bool HasNonWhiteSpaceContent([NotNullWhen(true)] this string? value) => !string.IsNullOrWhiteSpace(value);
+}
---
+++ test/Smab.Helpers.Test/MiscHelperTests/HasNonWhiteSpaceContentTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Smab.Helpers.Test.MiscHelperTests;
+public class HasNonWhiteSpaceContentTests {
+
+	[Theory]
+	[InlineData("abc")]
+	[InlineData(" abc")]
+	[InlineData("\tabc")]
+	[InlineData("\tabc\t")]
+	[InlineData("\tabc\nabc\n")]
+	public void HasNonWhiteSpaceContent_ShouldBe_True(string? value) {
+		value.HasNonWhiteSpaceContent().ShouldBeTrue();
+	}
+
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("    ")]
+	[InlineData("\t\n")]
+	public void HasNonWhiteSpaceContent_ShouldBe_False(string? value) {
+		value.HasNonWhiteSpaceContent().ShouldBeFalse();
+	}
+}
