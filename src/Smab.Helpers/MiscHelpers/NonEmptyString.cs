adc762398dc0d80d1304d2633ee5571ddc1246e6
smabuk <2011834+smabuk@users.noreply.github.com>
2024-01-18 11:27:35 +0000

    v1.5.8 NonEmptyString

---
+++ src/Smab.Helpers/MiscHelpers/NonEmptyString.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Smab.Helpers;
+
+/// <summary>
+/// This represents a string that is incapable of holding an empty string or one consisting purely of whitespace
+/// </summary>
+/// <param name="Value"></param>
+/// <exception cref="ArgumentNullException"></exception>
+public readonly record struct NonEmptyString(string Value) : IParsable<NonEmptyString> {
+	public string Value { get; init; } = 
+		!string.IsNullOrWhiteSpace(Value)
+		? Value.Trim()
+		: throw new ArgumentNullException("Value cannot be null or white space", nameof(Value));
+
+	public NonEmptyString() : this(string.Empty) { }
+
+	public static implicit operator string(NonEmptyString value) => value.Value;
+	public static implicit operator NonEmptyString(string value) => new(value);
+
+	public static NonEmptyString Parse(string s, IFormatProvider? provider) => new(s);
+	public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out NonEmptyString result) {
+		if (!string.IsNullOrWhiteSpace(s)) {
+			result = new(s);
+			return true;
+		}
+
+		result = default;
+		return false;
+	}
+}
---
+++ test/Smab.Helpers.Test/MiscHelperTests/NonEmptyStringTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Smab.Helpers.Test.MiscHelperTests;
+public class NonEmptyStringTests {
+
+	[Theory]
+	[InlineData("abc",     "abc")]
+	[InlineData(" abc",    "abc")]
+	[InlineData("\tabc",   "abc")]
+	[InlineData("\tabc\t", "abc")]
+	[InlineData("\tabc\nabc\n", "abc\nabc")]
+	public void NonEmptyString_ShouldBe_Valid(string value, string expected) {
+		((string)new NonEmptyString(value)).ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData("")]
+	[InlineData("    ")]
+	[InlineData("\t\n")]
+	public void NonEmptyString_ShouldThrow_ArgumentNullException(string value) {
+		Should.Throw<ArgumentNullException>(() => new NonEmptyString(value))
+		.Message
+			.ShouldEndWith("Value (Parameter 'Value cannot be null or white space')");
+	}
+}
