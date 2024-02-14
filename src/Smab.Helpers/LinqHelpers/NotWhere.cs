313f7f5c9876c9bcaf9c97b0a4a124cac43b4f33
smabuk <2011834+smabuk@users.noreply.github.com>
2024-02-14 12:49:29 +0000

    v1.5.10 NotWhere

---
+++ src/Smab.Helpers/LinqHelpers/NotWhere.cs
@@ -0,0 +1,6 @@
+﻿namespace Smab.Helpers;
+
+public static partial class LinqHelpers {
+	public static IEnumerable<TSource> NotWhere<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		=> source.Where(x => !predicate(x));
+}
---
+++ test/Smab.Helpers.Test/LinqHelperTests/NotWhere.cs
@@ -0,0 +1,11 @@
+﻿namespace Smab.Helpers.Test.LinqHelperTests;
+public class NotWhereTests {
+	[Fact]
+	public void NotWhere_Negates_Where() {
+		IEnumerable<int> ints = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+		ints.Count().ShouldBe(11);
+		ints.Where(x => x % 2 == 0).Count().ShouldBe(5);
+		ints.NotWhere(x => x % 2 == 0).Count().ShouldBe(6);
+	}
+}
