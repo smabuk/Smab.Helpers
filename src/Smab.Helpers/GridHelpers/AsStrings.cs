59db25771af2838d5b9ec5d365dbdf094e7f6d6c
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-02 11:11:21 +0000

    v1.6.2 ArrayHelpers AsStrings() and AsString()

---
+++ src/Smab.Helpers/GridHelpers/AsStrings.cs
@@ -0,0 +1,34 @@
+﻿namespace Smab.Helpers;
+
+public static partial class ArrayHelpers {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="array"></param>
+	/// <param name="replacements"></param>
+	/// <returns></returns>
+	public static IEnumerable<string> AsStrings<T>(this T[,] array, params IEnumerable<(string, string)> replacements) where T : struct {
+		StringBuilder line = new();
+		for (int r = 0; r <= array.GetUpperBound(ROW_DIMENSION); r++) {
+			line.Clear();
+			for (int c = 0; c <= array.GetUpperBound(COL_DIMENSION); c++) {
+				string cell = array[c, r].ToString() ?? "";
+				line.Append(cell);
+			}
+			if (replacements is not null) {
+				foreach ((string from, string to) in replacements) {
+					line = line.Replace(from, to);
+				}
+			}
+			yield return line.ToString();
+		}
+	}
+
+	public static string AsStringWithNewLines<T>(this T[,] array, params IEnumerable<(string, string)> replacements) where T : struct
+		=> string.Join(Environment.NewLine, AsStrings(array, replacements));
+
+	public static string AsString<T>(this T[,] array, string? separator = null, params IEnumerable<(string, string)> replacements) where T : struct
+		=> string.Join("", AsStrings(array, replacements));
+}
---
+++ tests/Smab.Helpers.Tests/GridHelperTests/AsStrings.cs
@@ -0,0 +1,78 @@
+﻿namespace Smab.Helpers.Tests.GridHelperTests;
+public class AsStrings {
+
+	[Theory]
+	[InlineData(new int[] { 1, 2, 3, 4, 5, 6 }
+		, 3, 2
+		, "123")]
+	[InlineData(new int[] { 1, 2, 3, 4, 5, 6 }
+		, 2, 3
+		, "12")]
+	public void AsStrings_Int_Should_Have_Shape(int[] input, int cols, int? rows, string expected) {
+		string[] actual = [..input.To2dArray(cols, rows).AsStrings()];
+		Assert.Equal(expected, actual[0]);
+	}
+
+	[Theory]
+	[InlineData(new int[] { 1, 2, 3, 4, 5, 6 }
+		, 3, 2
+		, """
+		123
+		456
+		""")]
+	[InlineData(new int[] { 1, 2, 3, 4, 5, 6 }
+		, 2, 3
+		, """
+		12
+		34
+		56
+		""")]
+	public void AsString_Int_Should_Have_Shape(int[] input, int cols, int? rows, string expected) {
+		string actual = input.To2dArray(cols, rows).AsStringWithNewLines();
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""123456""", 3, 2
+		, """
+		123
+		456
+		""")]
+	[InlineData("""123456""", 2, 3
+		, """
+		12
+		34
+		56
+		""")]
+	public void AsString_String_Should_Have_Shape(string input, int cols, int? rows, string expected) {
+		string actual = input.To2dArray(cols, rows).AsStringWithNewLines();
+		Assert.Equal(expected, actual);
+	}
+
+
+	[Theory]
+	[InlineData("""123456""", 3, 2
+		, """
+		12*
+		4.6
+		""")]
+	[InlineData("""123456""", 2, 3
+		, """
+		12
+		*4
+		.6
+		""")]
+	public void AsString_Char_Should_Have_Replacements(string input, int cols, int? rows, string expected) {
+		string actual = input.To2dArray(cols, rows).AsStringWithNewLines(replacements: [("3", "*"), ("5", ".")]);
+		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	[InlineData("""123456""", 3, 2, """12*4.6""")]
+	[InlineData("""123456""", 2, 3, """12*4.6""")]
+	public void AsString_Char_Should_Be_Raw_String(string input, int cols, int? rows, string expected) {
+		string actual = input.To2dArray(cols, rows).AsString(replacements: [("3", "*"), ("5", ".")]);
+		Assert.Equal(expected, actual);
+	}
+
+}
