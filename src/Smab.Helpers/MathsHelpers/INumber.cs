1abd598d897fd76b8fd5c64c2f06d779eb74601f
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-04 19:40:16 +0000

    v1.6.8 INumber Is..() Methods

---
+++ src/Smab.Helpers/MathsHelpers/INumber.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace Smab.Helpers;
+
+public static partial class MathsHelpers {
+	public static bool IsEven<T>    (this T value) where T : INumber<T> => T.IsEvenInteger(value);
+	public static bool IsOdd<T>     (this T value) where T : INumber<T> => T.IsOddInteger(value);
+	public static bool IsInteger<T> (this T value) where T : INumber<T> => T.IsInteger(value);
+	public static bool IsNaN<T>     (this T value) where T : INumber<T> => T.IsNaN(value);
+	public static bool IsNegative<T>(this T value) where T : INumber<T> => T.IsNegative(value);
+	public static bool IsPositive<T>(this T value) where T : INumber<T> => T.IsPositive(value);
+	public static bool IsZero<T>    (this T value) where T : INumber<T> => T.IsZero(value);
+}
---
+++ tests/Smab.Helpers.Tests/MathsHelperTests/INumber.cs
@@ -0,0 +1,30 @@
+﻿namespace Smab.Helpers.Tests.MathsHelperTests;
+public class INumber {
+	[Theory]
+	[InlineData(1, false)]
+	[InlineData(2, true)]
+	[InlineData(0, true)]
+	[InlineData(-1, false)]
+	public void IsEven_Int_ShouldBe(int number, bool expected) {
+		number.IsEven().ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData(1, true)]
+	[InlineData(2, false)]
+	[InlineData(0, false)]
+	[InlineData(-1, true)]
+	public void IsOdd_Int_ShouldBe(int number, bool expected) {
+		number.IsOdd().ShouldBe(expected);
+	}
+
+	[Theory]
+	[InlineData(1.0, true)]
+	[InlineData(2.0, false)]
+	[InlineData(0.0, false)]
+	[InlineData(0.7, false)]
+	[InlineData(-1.0, true)]
+	public void IsOdd_Float_ShouldBe(float number, bool expected) {
+		number.IsOdd().ShouldBe(expected);
+	}
+}
