a872d2aeaaf4d92d9e2f1c4f11ef3194877c759b
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-23 16:31:21 +0000

    v1.7.0 ManhattanDistance for 3d points

--- src/Smab.Helpers/AlgorithmicHelpers/ManhattanDistance.cs
+++ src/Smab.Helpers/AlgorithmicHelpers/ManhattanDistance.cs
@@ -2,6 +2,8 @@
 
 namespace Smab.Helpers;
 public static partial class AlgorithmicHelpers {
+
+// 2d
 	public static T ManhattanDistance<T>(this (T X, T Y) point1, (T X, T Y) point2) where T : INumber<T>
 		=> T.Abs(point1.X - point2.X) + T.Abs(point1.Y - point2.Y);
 
@@ -19,4 +21,25 @@ public static partial class AlgorithmicHelpers {
 
 	public static IEnumerable<long> ManhattanDistances(this IEnumerable<IEnumerable<Point>> listOfPoints)
 		=> listOfPoints.Select(pair => (long)pair.ManhattanDistance());
+
+
+// 3d
+	public static T ManhattanDistance<T>(this (T X, T Y, T Z) point1, (T X, T Y, T Z) point2) where T : INumber<T>
+		=> T.Abs(point1.X - point2.X) + T.Abs(point1.Y - point2.Y) + T.Abs(point1.Z - point2.Z);
+
+	public static T ManhattanDistance<T>(this IEnumerable<(T X, T Y, T Z)> points) where T : INumber<T>
+		=> points.Count() == 2
+			? T.Abs(points.First().X - points.Last().X) + T.Abs(points.First().Y - points.Last().Y + T.Abs(points.First().Z - points.Last().Z))
+			: throw new InvalidOperationException($"You have {points.Count()} elements and there must be exactly 2.");
+
+	public static int ManhattanDistance(this IEnumerable<Point3d> points)
+		=> points.Count() == 2
+			? int.Abs(points.First().X - points.Last().X) + int.Abs(points.First().Y - points.Last().Y) + int.Abs(points.First().Z - points.Last().Z)
+			: throw new InvalidOperationException($"You have {points.Count()} elements and there must be exactly 2.");
+
+	public static int ManhattanDistance(this Point3d point1, Point3d point2) => Math.Abs(point1.X - point2.X) + Math.Abs(point1.Y - point2.Y) + Math.Abs(point1.Z - point2.Z);
+
+	public static IEnumerable<long> ManhattanDistances(this IEnumerable<IEnumerable<Point3d>> listOfPoints)
+		=> listOfPoints.Select(pair => (long)pair.ManhattanDistance());
+
 }
--- tests/Smab.Helpers.Tests/AlgorithmHelperTests/AlgorithmHelperTests.cs
+++ tests/Smab.Helpers.Tests/AlgorithmHelperTests/AlgorithmHelperTests.cs
@@ -1,5 +1,5 @@
 ﻿namespace Smab.Helpers.Tests.AlgorithmHelperTests;
-public class AlgorithmHelperTests(ITestOutputHelper testOutputHelper) {
+public partial class AlgorithmHelperTests(ITestOutputHelper testOutputHelper) {
 	[Theory]
 	[InlineData((int[])[2, 3],    0, 1)]
 	[InlineData((int[])[2, 3],    1, 2)]
@@ -70,70 +70,4 @@ public class AlgorithmHelperTests(ITestOutputHelper testOutputHelper) {
 		actual.ShouldBe(expected);
 	}
 
-
-
-
-
-	[Theory]
-	[InlineData(1, 2, 3, 4, 4)]
-	[InlineData(100, 200, 385, 3424, 3509)]
-	public void ManhattanDistance_Points_ShouldBe(int x1, int y1, int x2, int y2, int expected) {
-
-		Point point1 = new(x1, y1);
-		Point point2 = new(x2, y2);
-		long actual = point1.ManhattanDistance(point2);
-		actual.ShouldBe(expected);
-	}
-
-	[Theory]
-	[InlineData(1, 2, 3, 4, 4)]
-	[InlineData(100, 200, 385, 3424, 3509)]
-	public void ManhattanDistance_Ints_ShouldBe(int x1, int y1, int x2, int y2, int expected) {
-
-		long actual = (x1, y1).ManhattanDistance((x2, y2));
-		actual.ShouldBe(expected);
-	}
-
-	[Theory]
-	[InlineData(1, 2, 3, 4, 4)]
-	[InlineData(100, 200, 385, 3424, 3509)]
-	public void ManhattanDistance_Longs_ShouldBe(long x1, long y1, long x2, long y2, long expected) {
-
-		long actual = (x1, y1).ManhattanDistance((x2, y2));
-		actual.ShouldBe(expected);
-	}
-
-	[Theory]
-	[InlineData(1, 2, 3, 4, 4)]
-	[InlineData(100, 200, 385, 3424, 3509)]
-	public void ManhattanDistance_Pairs_ShouldBe(long x1, long y1, long x2, long y2, long expected) {
-
-		IEnumerable<(long, long)> pairs = [(x1, y1), (x2, y2)];
-		long actual = pairs.ToArray().ManhattanDistance();
-		actual.ShouldBe(expected);
-	}
-
-	[Fact]
-	public void ManhattanDistance_Pairs_Of_Points_ShouldBe() {
-		List<Point> points = [new(1,4), new(87,32), new(43, -2), new(12, 5), new(-88, 4), new(3,9)];
-
-		IEnumerable<IEnumerable<Point>> pairs = points.Chunk(2);
-		List<long> actual = [..pairs.ManhattanDistances()];
-		actual.ShouldBe([114, 38, 96]);
-		pairs.First().ManhattanDistance().ShouldBe(114);
-	}
-
-	[Fact]
-	public void ManhattanDistance_Should_Throw() {
-
-		IEnumerable<(long, long)> pairs = [(1, 2), (3, 4), (5, 6)];
-		Should.Throw<InvalidOperationException>(() => pairs.ToArray().ManhattanDistance())
-		.Message
-			.ShouldEndWith("You have 3 elements and there must be exactly 2.");
-
-		IEnumerable<Point> pair = [new(1, 2), new(3, 4), new(5, 6)];
-		Should.Throw<InvalidOperationException>(() => pairs.ToArray().ManhattanDistance())
-		.Message
-			.ShouldEndWith("You have 3 elements and there must be exactly 2.");
-	}
 }
