5cec28474dc23bbac073e423e9649c9eb3a04f9d
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-23 17:07:57 +0000

    v1.7.1 GridHelper TryGetValue

---
+++ src/Smab.Helpers/GridHelpers/TryGetValue.cs
@@ -0,0 +1,34 @@
+﻿namespace Smab.Helpers;
+
+public static partial class ArrayHelpers {
+
+	public static bool TryGetValue<T>(this T[,] array, int x, int y, out T value) {
+		value = default!;
+		if (array.IsOutOfBounds(x, y)) {
+			return false;
+		}
+
+		value = array[x, y];
+		return true;
+	}
+
+	public static bool TryGetValue<T>(this T[,] array, (int X, int Y) point, out T value) {
+		value = default!;
+		if (array.IsOutOfBounds(point.X, point.Y)) {
+			return false;
+		}
+
+		value = array[point.X, point.Y];
+		return true;
+	}
+
+	public static bool TryGetValue<T>(this T[,] array, Point point, out T value) {
+		value = default!;
+		if (array.IsOutOfBounds(point.X, point.Y)) {
+			return false;
+		}
+
+		value = array[point.X, point.Y];
+		return true;
+	}
+}
---
+++ tests/Smab.Helpers.Tests/GridHelperTests/TryGetValue.cs
@@ -0,0 +1,18 @@
+﻿namespace Smab.Helpers.Tests.GridHelperTests;
+public class TryGetValue {
+
+	private static readonly char[,] INPUT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".To2dArray(5);
+
+	[Theory]
+	[InlineData(0, 0, true, 'A')]
+	[InlineData(1, 1, true, 'G')]
+	[InlineData(4, 3, true, 'T')]
+	[InlineData(6, 6, false)]
+	public void TryGetValue_Should_Have(int X, int Y, bool isInBounds, char expected = ' ') {
+		bool inBounds = INPUT.TryGetValue(X, Y, out char actual);
+		inBounds.ShouldBe(isInBounds);
+		if (inBounds) {
+			actual.ShouldBe(expected);
+		}
+	}
+}
