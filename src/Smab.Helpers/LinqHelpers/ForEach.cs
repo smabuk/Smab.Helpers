23d5ea0e1abc01e4aca83b666b08439d627ea50d
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-24 11:31:52 +0000

    v1.7.2 IEnumerable ForEach

---
+++ src/Smab.Helpers/LinqHelpers/ForEach.cs
@@ -0,0 +1,9 @@
+﻿namespace Smab.Helpers;
+
+public static partial class LinqHelpers {
+	public static void ForEach<T>(this IEnumerable<T> items, Action<T> action) {
+		foreach (T? item in items) {
+			action(item);
+		}
+	}
+}
---
+++ tests/Smab.Helpers.Tests/LinqHelperTests/ForEach.cs
@@ -0,0 +1,20 @@
+﻿namespace Smab.Helpers.Tests.LinqHelperTests;
+public class ForEach {
+	private static readonly IEnumerable<int>    ints    = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+	[Fact]
+	public void ForEach_Ints_Should_Return() {
+		int actual = 0;
+		ints.ForEach(i => { actual += i; });
+		actual.ShouldBe(66);
+	}
+
+	[Fact]
+	public void ForEach_Ints_Arrray_Should_Return() {
+		int[] intArray = [.. ints];
+		int actual = 0;
+		intArray.ForEach(i => { actual += i; });
+		actual.ShouldBe(66);
+	}
+
+}
