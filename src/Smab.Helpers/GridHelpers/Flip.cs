d301372b5a88cbf436d7d71717b65fc67a4e4560
smabuk <2011834+smabuk@users.noreply.github.com>
2024-11-24 17:53:48 +0000

    v1.7.4 Array Flip

---
+++ src/Smab.Helpers/GridHelpers/Flip.cs
@@ -0,0 +1,32 @@
+﻿namespace Smab.Helpers;
+public static partial class ArrayHelpers {
+	public static T[,] FlipHorizontally<T>(this T[,] array) {
+		int colsCount = array.ColsCount();
+		int rowsCount = array.RowsCount();
+
+		T[,] result = new T[colsCount, rowsCount];
+
+		for (int row = 0; row < rowsCount; row++) {
+			for (int col = 0; col < colsCount; col++) {
+				result[col, row] = array[colsCount - col - 1, row];
+			}
+		}
+
+		return result;
+	}
+
+	public static T[,] FlipVertically<T>(this T[,] array) {
+		int colsCount = array.ColsCount();
+		int rowsCount = array.RowsCount();
+
+		T[,] result = new T[colsCount, rowsCount];
+
+		for (int row = 0; row < rowsCount; row++) {
+			for (int col = 0; col < colsCount; col++) {
+				result[col, row] = array[col, rowsCount - row - 1];
+			}
+		}
+
+		return result;
+	}
+}
---
+++ tests/Smab.Helpers.Tests/GridHelperTests/Flip.cs
@@ -0,0 +1,34 @@
+﻿namespace Smab.Helpers.Tests.GridHelperTests;
+public class Flip(ITestOutputHelper testOutputHelper) {
+
+	private static readonly char[,] INPUT_ARRAY = """
+		A.B.1
+		CD..2
+		E.FG.
+		HIJ.3
+		""".Split(Environment.NewLine).To2dArray();
+
+	[Fact]
+	public void Flip_Horizontally() {
+		char[,] array = INPUT_ARRAY.FlipHorizontally();
+		string actual = string.Join(Environment.NewLine, array.AsStrings());
+		actual.ShouldBe("""
+			1.B.A
+			2..DC
+			.GF.E
+			3.JIH
+			""");
+	}
+
+	[Fact]
+	public void Flip_Vertically() {
+		char[,] array = INPUT_ARRAY.FlipVertically();
+		string actual = string.Join(Environment.NewLine, array.AsStrings());
+		actual.ShouldBe("""
+			HIJ.3
+			E.FG.
+			CD..2
+			A.B.1
+			""");
+	}
+}
