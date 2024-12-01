a7030901bfdb93758f610a4e5d85f4f02965aab2
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-01 13:28:24 +0000

    v1.7.8 SplitOnWhitespace

---
+++ src/Smab.Helpers/ParsingHelpers/SplitOnWhitespace.cs
@@ -0,0 +1,11 @@
+﻿namespace Smab.Helpers;
+public static partial class ParsingHelpers {
+	private static readonly string[] WHITESPACE_SEPARATORS = [" ",  "\t"];
+	private static readonly string[] WHITESPACE_SEPARATORS_WITH_NEWLINE = [..WHITESPACE_SEPARATORS, Environment.NewLine];
+
+	public static string[] SplitOnWhitespace(this string s, bool treatNewlineAsWhitespace = true) =>
+			s.TrimmedSplit(treatNewlineAsWhitespace ? WHITESPACE_SEPARATORS_WITH_NEWLINE : WHITESPACE_SEPARATORS);
+
+	public static string[] SplitOnWhitespace(this string s, int count, bool treatNewlineAsWhitespace = true) =>
+		s.TrimmedSplit(treatNewlineAsWhitespace ? WHITESPACE_SEPARATORS_WITH_NEWLINE : WHITESPACE_SEPARATORS, count);
+}
