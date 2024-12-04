50c3ebf4a755b54507bc3f1901b013a9b88933e3
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-04 08:03:49 +0000

    v1.7.10 Rename Walk to ForEach
    * deprecate Walk..

---
+++ src/Smab.Helpers/GridHelpers/ForEach.cs
@@ -0,0 +1,52 @@
+﻿namespace Smab.Helpers;
+
+public static partial class ArrayHelpers {
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="array"></param>
+	/// <returns></returns>
+	public static IEnumerable<Cell<T>> ForEachCell<T>(this T[,] array) {
+		int cols = array.GetUpperBound(COL_DIMENSION);
+		int rows = array.GetUpperBound(ROW_DIMENSION);
+
+		for (int row = 0; row <= rows; row++) {
+		for (int col = 0; col <= cols; col++) {
+			yield return new(col, row, array[col, row]);
+		}}
+	}
+
+	public static IEnumerable<(int X, int Y)> ForEach<T>(this T[,] array) {
+		int cols = array.GetUpperBound(COL_DIMENSION);
+		int rows = array.GetUpperBound(ROW_DIMENSION);
+
+		for (int row = 0; row <= rows; row++) {
+		for (int col = 0; col <= cols; col++) {
+			yield return new(col, row);
+		}}
+	}
+
+	[Obsolete("Use ForEachCell instead", false)]
+	public static IEnumerable<Cell<T>> WalkWithValues<T>(this T[,] array) {
+		int cols = array.GetUpperBound(COL_DIMENSION);
+		int rows = array.GetUpperBound(ROW_DIMENSION);
+
+		for (int row = 0; row <= rows; row++) {
+			for (int col = 0; col <= cols; col++) {
+				yield return new(col, row, array[col, row]);
+			}
+		}
+	}
+
+	[Obsolete("Use ForEach instead", false)]
+	public static IEnumerable<(int X, int Y)> Walk<T>(this T[,] array) {
+		int cols = array.GetUpperBound(COL_DIMENSION);
+		int rows = array.GetUpperBound(ROW_DIMENSION);
+
+		for (int row = 0; row <= rows; row++) {
+		for (int col = 0; col <= cols; col++) {
+			yield return new(col, row);
+		}}
+	}
+}
--- tests/Smab.Helpers.Tests/GridHelperTests/ForEach.cs
+++ tests/Smab.Helpers.Tests/GridHelperTests/ForEach.cs
@@ -1,16 +1,16 @@
 ﻿namespace Smab.Helpers.Tests.GridHelperTests;
-public class Walk {
+public class ForEach {
 	[Fact]
-	public void Walk2dArray_Should_Walk_Across_Then_Down() {
+	public void ForEach_2dArray_Should_Travel_Across_Then_Down() {
 		Point expectedPoint;
 		(char, int)[] input = new (char, int)[26];
 		for (int i = 0; i < input.GetUpperBound(0); i++) {
 			input[i] = new((char)(65 + i), i + 1);
 		}
 		(char, int)[,] array = input.To2dArray(5);
-		expectedPoint = array.Walk().Select(p => new Point(p.X, p.Y)).Skip(7).First();
+		expectedPoint = array.ForEach().Select(p => new Point(p.X, p.Y)).Skip(7).First();
 		Assert.Equal((2, 1), expectedPoint);
-		(int x, int y, (char, int) value) = array.WalkWithValues().Skip(7).First();
+		(int x, int y, (char, int) value) = array.ForEachCell().Skip(7).First();
 		Assert.Equal(2, x);
 		Assert.Equal(1, y);
 		Assert.Equal('H', value.Item1);
