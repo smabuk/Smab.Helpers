6431f9ae4d0c3758125e683de11196f7c9fcf730
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-10 06:58:00 +0000

    v1.7.17 AsDigitsOrDefault

--- src/Smab.Helpers/ParsingHelpers/AsDigits.cs
+++ src/Smab.Helpers/ParsingHelpers/AsDigits.cs
@@ -8,4 +8,9 @@ public static partial class ParsingHelpers {
 
 	public static IEnumerable<T> AsDigits<T>(this IEnumerable<string> s, IFormatProvider? provider = null) where T : INumber<T> =>
 		s.Select(x => T.Parse($"{x}", provider));
+
+
+
+	public static IEnumerable<T> AsDigitsOrDefault<T>(this string s, T? defaultValue, IFormatProvider? provider = null) where T : INumber<T> =>
+		s.Select(x => char.IsAsciiDigit(x) ? T.Parse($"{x}", provider) : defaultValue ?? T.Zero);
 }
--- tests/Smab.Helpers.Tests/ParsingHelperTests/AsDigits.cs
+++ tests/Smab.Helpers.Tests/ParsingHelperTests/AsDigits.cs
@@ -16,4 +16,18 @@ public class AsDigits {
 		string[] doublesAsStrings = ["01234.34", "56789.56", "-234.99"];
 		doublesAsStrings.AsDigits<double>().ShouldBe([1234.34, 56789.56, -234.99]);
 	}
+
+	[Fact]
+	public void AsDigitsOrDefaults_Tests() {
+		string digits = "045678";
+		string badDigits = "045a7b";
+
+		digits.AsDigitsOrDefault<int>(-1).ShouldBe([0, 4, 5, 6, 7, 8]);
+		digits.AsDigitsOrDefault<long>(-1).ShouldBe([0, 4, 5, 6, 7, 8]);
+		digits.AsDigitsOrDefault<double>(-1).ShouldBe([0.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
+
+		digits.AsDigitsOrDefault<char>('!').ShouldBe(['0','4', '5', '6', '7', '8']);
+
+		badDigits.AsDigitsOrDefault<int>(-1).ShouldBe([0, 4, 5, -1, 7, -1]);
+	}
 }
