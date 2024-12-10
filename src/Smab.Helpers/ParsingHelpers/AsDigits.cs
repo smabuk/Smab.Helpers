5a4f7ee0894919a0d4a8bfffdad7c0381421f8f0
smabuk <2011834+smabuk@users.noreply.github.com>
2024-12-10 06:58:00 +0000

    v1.7.17 AsDigitsOrDefault

--- src/Smab.Helpers/ParsingHelpers/AsDigits.cs
+++ src/Smab.Helpers/ParsingHelpers/AsDigits.cs
@@ -8,4 +8,16 @@ public static partial class ParsingHelpers {
 
 	public static IEnumerable<T> AsDigits<T>(this IEnumerable<string> s, IFormatProvider? provider = null) where T : INumber<T> =>
 		s.Select(x => T.Parse($"{x}", provider));
+
+
+
+	public static IEnumerable<T> AsDigitsOrDefault<T>(this string s, T? defaultValue, IFormatProvider? provider = null) where T : INumber<T> {
+		for (int i = 0; i < s.Length; i++) {
+			if (T.TryParse($"{s[i]}", provider, out T? value)) {
+				yield return value;
+			} else {
+				yield return defaultValue ?? T.Zero;
+			}
+		}
+	}
 }
