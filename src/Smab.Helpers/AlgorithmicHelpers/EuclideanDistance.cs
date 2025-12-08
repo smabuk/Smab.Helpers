cc6550d1609936885291aeed0fe5e2c33193c802
smabuk <2011834+smabuk@users.noreply.github.com>
2025-12-08 08:08:20 +0000

    v1.10.4 Add Euclidean distance extensions and tests
    Introduced `AlgorithmicHelpers` with extension methods for
    calculating Euclidean distances in 2D and 3D spaces. Supported
    various data types, including tuples, `Point`, `Point3d`, and
    `IEnumerable` collections. Added validation to ensure exactly
    two elements in collections for distance calculations.
    
    Implemented comprehensive unit tests in
    `Smab.Helpers.Tests.AlgorithmHelperTests` to cover all
    scenarios, including edge cases and invalid inputs. Used
    `ShouldBe` assertions with high precision to validate results.

---
+++ src/Smab.Helpers/AlgorithmicHelpers/EuclideanDistance.cs
@@ -0,0 +1,92 @@
+﻿namespace Smab.Helpers;
+
+public static partial class AlgorithmicHelpers {
+
+	extension<T>((T X, T Y) point1) where T : INumber<T> {
+		// 2d
+		public double EuclideanDistance((T X, T Y) point2) {
+			double x1 = double.CreateChecked(point1.X), y1 = double.CreateChecked(point1.Y);
+			double x2 = double.CreateChecked(point2.X), y2 = double.CreateChecked(point2.Y);
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+		}
+	}
+
+	extension<T>(IEnumerable<(T X, T Y)> points) where T : INumber<T> {
+		public double EuclideanDistance() {
+			if (points.Count() != 2) {
+				throw new InvalidOperationException($"You have {points.Count()} elements and there must be exactly 2.");
+			}
+			double x1 = double.CreateChecked(points.First().X), y1 = double.CreateChecked(points.First().Y);
+			double x2 = double.CreateChecked(points.Last().X), y2 = double.CreateChecked(points.Last().Y);
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+		}
+	}
+
+	extension(IEnumerable<Point> points) {
+		public double EuclideanDistance() {
+			if (points.Count() != 2) {
+				throw new InvalidOperationException($"You have {points.Count()} elements and there must be exactly 2.");
+			}
+			double x1 = points.First().X, y1 = points.First().Y;
+			double x2 = points.Last().X, y2 = points.Last().Y;
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+		}
+	}
+
+	extension(Point point1) {
+		public double EuclideanDistance(Point point2) {
+			double x1 = point1.X, y1 = point1.Y;
+			double x2 = point2.X, y2 = point2.Y;
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+		}
+	}
+
+	extension(IEnumerable<IEnumerable<Point>> listOfPoints) {
+		public IEnumerable<double> EuclideanDistances()
+		=> listOfPoints.Select(pair => pair.EuclideanDistance());
+	}
+
+	extension<T>((T X, T Y, T Z) point1) where T : INumber<T> {
+		// 3d
+		public double EuclideanDistance((T X, T Y, T Z) point2) {
+			double x1 = double.CreateChecked(point1.X), y1 = double.CreateChecked(point1.Y), z1 = double.CreateChecked(point1.Z);
+			double x2 = double.CreateChecked(point2.X), y2 = double.CreateChecked(point2.Y), z2 = double.CreateChecked(point2.Z);
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+		}
+	}
+
+	extension<T>(IEnumerable<(T X, T Y, T Z)> points) where T : INumber<T> {
+		public double EuclideanDistance() {
+			if (points.Count() != 2) {
+				throw new InvalidOperationException($"You have {points.Count()} elements and there must be exactly 2.");
+			}
+			double x1 = double.CreateChecked(points.First().X), y1 = double.CreateChecked(points.First().Y), z1 = double.CreateChecked(points.First().Z);
+			double x2 = double.CreateChecked(points.Last().X), y2 = double.CreateChecked(points.Last().Y), z2 = double.CreateChecked(points.Last().Z);
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+		}
+	}
+
+	extension(IEnumerable<Point3d> points) {
+		public double EuclideanDistance() {
+			if (points.Count() != 2) {
+				throw new InvalidOperationException($"You have {points.Count()} elements and there must be exactly 2.");
+			}
+			double x1 = points.First().X, y1 = points.First().Y, z1 = points.First().Z;
+			double x2 = points.Last().X, y2 = points.Last().Y, z2 = points.Last().Z;
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+		}
+	}
+
+	extension(Point3d point1) {
+		public double EuclideanDistance(Point3d point2) {
+			double x1 = point1.X, y1 = point1.Y, z1 = point1.Z;
+			double x2 = point2.X, y2 = point2.Y, z2 = point2.Z;
+			return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+		}
+	}
+
+	extension(IEnumerable<IEnumerable<Point3d>> listOfPoints) {
+		public IEnumerable<double> EuclideanDistances()
+		=> listOfPoints.Select(pair => pair.EuclideanDistance());
+	}
+}
---
+++ tests/Smab.Helpers.Tests/AlgorithmHelperTests/EuclideanDistance.cs
@@ -0,0 +1,169 @@
+﻿namespace Smab.Helpers.Tests.AlgorithmHelperTests;
+
+public partial class AlgorithmHelperTests {
+	[Theory]
+	[InlineData(0, 0, 3, 4, 5.0)]
+	[InlineData(1, 2, 4, 6, 5.0)]
+	[InlineData(0, 0, 1, 1, 1.4142135623730951)]
+	[InlineData(100, 200, 385, 3424, 3236.5724153801966)]
+	public void EuclideanDistance_Points_ShouldBe(int x1, int y1, int x2, int y2, double expected) {
+
+		Point point1 = new(x1, y1);
+		Point point2 = new(x2, y2);
+		double actual = point1.EuclideanDistance(point2);
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0, 0, 3, 4, 5.0)]
+	[InlineData(1, 2, 4, 6, 5.0)]
+	[InlineData(0, 0, 1, 1, 1.4142135623730951)]
+	[InlineData(100, 200, 385, 3424, 3236.5724153801966)]
+	public void EuclideanDistance_Ints_ShouldBe(int x1, int y1, int x2, int y2, double expected) {
+
+		double actual = (x1, y1).EuclideanDistance((x2, y2));
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0, 0, 3, 4, 5.0)]
+	[InlineData(1, 2, 4, 6, 5.0)]
+	[InlineData(0, 0, 1, 1, 1.4142135623730951)]
+	[InlineData(100, 200, 385, 3424, 3236.5724153801966)]
+	public void EuclideanDistance_Longs_ShouldBe(long x1, long y1, long x2, long y2, double expected) {
+
+		double actual = (x1, y1).EuclideanDistance((x2, y2));
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0.0, 0.0, 3.0, 4.0, 5.0)]
+	[InlineData(1.5, 2.5, 4.5, 6.5, 5.0)]
+	[InlineData(0.0, 0.0, 1.0, 1.0, 1.4142135623730951)]
+	public void EuclideanDistance_Doubles_ShouldBe(double x1, double y1, double x2, double y2, double expected) {
+
+		double actual = (x1, y1).EuclideanDistance((x2, y2));
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0, 0, 3, 4, 5.0)]
+	[InlineData(1, 2, 4, 6, 5.0)]
+	[InlineData(0, 0, 1, 1, 1.4142135623730951)]
+	[InlineData(100, 200, 385, 3424, 3236.5724153801966)]
+	public void EuclideanDistance_Pairs_ShouldBe(long x1, long y1, long x2, long y2, double expected) {
+
+		IEnumerable<(long, long)> pairs = [(x1, y1), (x2, y2)];
+		double actual = pairs.EuclideanDistance();
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Fact]
+	public void EuclideanDistance_Pairs_Of_Points_ShouldBe() {
+		List<Point> points = [new(0, 0), new(3, 4), new(10, 10), new(20, 20), new(0, 5), new(12, 0)];
+
+		IEnumerable<IEnumerable<Point>> pairs = points.Chunk(2);
+		List<double> actual = [.. pairs.EuclideanDistances()];
+		actual[0].ShouldBe(5.0, 0.0000001);
+		actual[1].ShouldBe(14.142135623730951, 0.0000001);
+		actual[2].ShouldBe(13.0, 0.0000001);
+		pairs.First().EuclideanDistance().ShouldBe(5.0, 0.0000001);
+	}
+
+	[Fact]
+	public void EuclideanDistance_Should_Throw() {
+
+		IEnumerable<(long, long)> pairs = [(1, 2), (3, 4), (5, 6)];
+		Should.Throw<InvalidOperationException>(() => pairs.EuclideanDistance())
+		.Message
+			.ShouldEndWith("You have 3 elements and there must be exactly 2.");
+
+		IEnumerable<Point> pair = [new(1, 2), new(3, 4), new(5, 6)];
+		Should.Throw<InvalidOperationException>(() => pair.EuclideanDistance())
+		.Message
+			.ShouldEndWith("You have 3 elements and there must be exactly 2.");
+	}
+
+	// 3d
+
+	[Theory]
+	[InlineData(0, 0, 0, 3, 4, 0, 5.0)]
+	[InlineData(0, 0, 0, 1, 1, 1, 1.7320508075688772)]
+	[InlineData(1, 2, 3, 4, 5, 6, 5.196152422706632)]
+	[InlineData(100, 200, 385, 3424, 888, 999, 3449.5385198603017)]
+	public void EuclideanDistance_Points3d_ShouldBe(int x1, int y1, int z1, int x2, int y2, int z2, double expected) {
+
+		Point3d point1 = new(x1, y1, z1);
+		Point3d point2 = new(x2, y2, z2);
+		double actual = point1.EuclideanDistance(point2);
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0, 0, 0, 3, 4, 0, 5.0)]
+	[InlineData(0, 0, 0, 1, 1, 1, 1.7320508075688772)]
+	[InlineData(1, 2, 3, 4, 5, 6, 5.196152422706632)]
+	[InlineData(100, 200, 385, 3424, 888, 999, 3449.5385198603017)]
+	public void EuclideanDistance_Ints3d_ShouldBe(int x1, int y1, int z1, int x2, int y2, int z2, double expected) {
+
+		double actual = (x1, y1, z1).EuclideanDistance((x2, y2, z2));
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0, 0, 0, 3, 4, 0, 5.0)]
+	[InlineData(0, 0, 0, 1, 1, 1, 1.7320508075688772)]
+	[InlineData(1, 2, 3, 4, 5, 6, 5.196152422706632)]
+	public void EuclideanDistance_Longs3d_ShouldBe(long x1, long y1, long z1, long x2, long y2, long z2, double expected) {
+
+		double actual = (x1, y1, z1).EuclideanDistance((x2, y2, z2));
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0.0, 0.0, 0.0, 3.0, 4.0, 0.0, 5.0)]
+	[InlineData(0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.7320508075688772)]
+	[InlineData(1.5, 2.5, 3.5, 4.5, 6.5, 9.5, 7.810249675906654)]
+	public void EuclideanDistance_Doubles3d_ShouldBe(double x1, double y1, double z1, double x2, double y2, double z2, double expected) {
+
+		double actual = (x1, y1, z1).EuclideanDistance((x2, y2, z2));
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Theory]
+	[InlineData(0, 0, 0, 3, 4, 0, 5.0)]
+	[InlineData(0, 0, 0, 1, 1, 1, 1.7320508075688772)]
+	[InlineData(1, 2, 3, 4, 5, 6, 5.196152422706632)]
+	public void EuclideanDistance_Pairs3d_ShouldBe(long x1, long y1, long z1, long x2, long y2, long z2, double expected) {
+
+		IEnumerable<(long, long, long)> pairs = [(x1, y1, z1), (x2, y2, z2)];
+		double actual = pairs.EuclideanDistance();
+		actual.ShouldBe(expected, 0.0000001);
+	}
+
+	[Fact]
+	public void EuclideanDistance_Pairs_Of_Points3d_ShouldBe() {
+		List<Point3d> points = [new(0, 0, 0), new(3, 4, 0), new(1, 1, 1), new(4, 5, 6), new(10, 0, 0), new(0, 10, 0)];
+
+		IEnumerable<IEnumerable<Point3d>> pairs = points.Chunk(2);
+		List<double> actual = [.. pairs.EuclideanDistances()];
+		actual[0].ShouldBe(5.0, 0.0000001);
+		actual[1].ShouldBe(7.0710678118654755, 0.0000001);
+		actual[2].ShouldBe(14.142135623730951, 0.0000001);
+		pairs.First().EuclideanDistance().ShouldBe(5.0, 0.0000001);
+	}
+
+	[Fact]
+	public void EuclideanDistance3d_Should_Throw() {
+
+		IEnumerable<(long, long, long)> pairs = [(1, 2, 3), (4, 5, 6), (7, 8, 9)];
+		Should.Throw<InvalidOperationException>(() => pairs.EuclideanDistance())
+		.Message
+			.ShouldEndWith("You have 3 elements and there must be exactly 2.");
+
+		IEnumerable<Point3d> pair = [new(1, 2, 3), new(4, 5, 6), new(7, 8, 9)];
+		Should.Throw<InvalidOperationException>(() => pair.EuclideanDistance())
+		.Message
+			.ShouldEndWith("You have 3 elements and there must be exactly 2.");
+	}
+}
